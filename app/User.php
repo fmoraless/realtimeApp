5d1b0b344d97db7f9a9a5d2e76c8193a0049d0b0
fmoraless <franticzero@gmail.com>
2019-10-04 12:58:45 -0300

    Función encritar password de usuario.

--- app/User.php
+++ app/User.php
@@ -64,4 +64,9 @@ class User extends Authenticatable implements JWTSubject
         return [];
     }
 
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
+
 }
